fb9c3e0745df46d95940f8093985932f59b2fade
Vivek Pawar <pawarvivek29@gmail.com>
2020-08-26 09:37:02 +0530

    model modification

--- app/core/models.py
+++ app/core/models.py
@@ -27,6 +27,7 @@ class User(AbstractBaseUser, PermissionsMixin):
   """Custom user model that supports using email instead of username"""
   email = models.EmailField(max_length=255, unique=True)
   name = models.CharField(max_length=255)
+  mobile = models.IntegerField(null=True)
   is_active = models.BooleanField(default=True)
   is_staff = models.BooleanField(default=False)
 
