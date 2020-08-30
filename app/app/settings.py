124a28004efaf2e908e251e016025283b8ba5870
Vivek Pawar <pawarvivek29@gmail.com>
2020-08-31 00:48:19 +0530

    authenticating user

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'rest_framework',
+    'rest_framework.authtoken',
+    'core',
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +79,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'testdb',
+        'USER': 'testuser',
+        'PASSWORD': 'test',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
