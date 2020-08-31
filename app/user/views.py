80cce4c1ca42c7ad5cabeedd4d4d2cf37d03e83a
Vivek Pawar <pawarvivek29@gmail.com>
2020-09-01 00:45:09 +0530

    added working list tag feature

--- app/user/views.py
+++ app/user/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from . serializers import UserSerializer, AuthTokenSerializer
@@ -12,3 +12,13 @@ class CreateTokenView(ObtainAuthToken):
   """Create a new auth token for user"""
   serializer_class = AuthTokenSerializer
   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+  """Manage the authentication user"""
+  serializer_class = UserSerializer
+  authentication_classes = (authentication.TokenAuthentication,)
+  permission_classes = (permissions.IsAuthenticated,)
+
+  def get_object(self):
+    """Retrieve & return authentication user"""
+    return self.request.user
